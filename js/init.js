626dc2b7b8b2ddc13c950d5117a3dfeb25ca378d
Ernest96 <bitca@inbox.ru>
2026-02-04 12:59:03 +0200

    update

--- js/init.js
+++ js/init.js
@@ -1,14 +1,33 @@
 //Hook up the tweet display
 
-$(document).ready(function() {
-						   
-	$(".countdown").countdown({
-				date: "8 July 2048 18:30:00",
-				format: "on"
-			},
-			
-			function() {
-				// callback function
-			});
+$(document).ready(function () {
 
-});	
+	$(".countdown").countdown({
+		date: "8 July 2048 18:30:00",
+		format: "on"
+	},
+
+		function () {
+			// callback function
+		});
+
+});
+
+function data() {
+	var div;
+	var date = new Date();
+	var an, luna, zi;
+	var text;
+
+	an = date.getFullYear();
+	luna = date.getMonth() + 1;
+	zi = date.getDate();
+	text = zi + "." + luna + "." + an;
+
+	div = document.getElementById("data");
+	if (div) {
+		div.innerHTML = "  ®Copyright" + " " + "King-Cleaning" + " " + an;
+	}
+}
+
+data(); 
